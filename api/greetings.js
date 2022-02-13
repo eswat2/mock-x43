87e63ca76d63851d700de5e9237e1b065f929727
Richard Hess <eswat2@gmail.com>
2022-02-13 11:41:27 -0800

    add vins api

--- api/greetings.js
+++ api/greetings.js
@@ -2,6 +2,6 @@ module.exports = (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.json({
     message: 'hooray! welcome to our api server!...',
-    apis: ['hash', 'lorem', 'nid', 'sfid', 'sid', 'slug', 'uuid'],
+    apis: ['hash', 'lorem', 'nid', 'sfid', 'sid', 'slug', 'uuid', 'vins'],
   })
 }
---
+++ api/vins.js
@@ -0,0 +1,11 @@
+import vinGenerator from 'vin-generator'
+import { chance } from '../utils/mocks'
+
+module.exports = (req, res) => {
+  const { count } = req.query
+  const num = count ? parseInt(count) : 3
+  const vins = chance.unique(vinGenerator.generateVin, num)
+
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.json(vins)
+}
