4068b220bd5adb49dc2a5fd5d2bfa0a8e3f3f212
Richard Hess <eswat2@gmail.com>
2022-06-13 14:31:01 -0700

    refactor tests

--- tests/mocks.js
+++ tests/mocks.js
@@ -1,11 +1,21 @@
 import { chance, delay, nid, randomArray, sfid, sid, slug, uuid } from '../utils/mocks.js'
 import { expect } from 'chai'
 
+const checkNumber = (data, min, max) => {
+  it('should return a number', () => {
+    expect(data).to.be.a('number')
+  })
+  it(`should be between ${min}-${max}`, () => {
+    expect(data).to.be.gte(min)
+    expect(data).to.be.lte(max)
+  })
+}
+
 const checkString = (data, limit) => {
   it('should return a string', () => {
     expect(data).to.be.a('string')
   })
-  it(`should return ${limit} chars`, () => {
+  it(`should be ${limit} chars`, () => {
     expect(data.length).to.equal(limit)
   })
 }
@@ -19,14 +29,10 @@ describe('chance.hash', () => {
 
 describe('delay', () => {
   const data = delay()
+  const min = 500
+  const max = 3500
   console.log('-- delay:', data)
-  it('should return a number', () => {
-    expect(data).to.be.a('number')
-  })
-  it('should return between 500-3500', () => {
-    expect(data).to.gte(500)
-    expect(data).to.lte(3500)
-  })
+  checkNumber(data, min, max)
 })
 
 describe('nid', () => {
@@ -38,20 +44,18 @@ describe('nid', () => {
 
 describe('randomArray', () => {
   const count = 10;
+  const min = 0;
   const max = 20;
   const data = randomArray(count, max)
   console.log('-- randomArray:', data.toString())
   it('should return an array', () => {
     expect(data).to.be.a('array')
   })
-  it(`should return ${count} items`, () => {
+  it(`should be ${count} items`, () => {
     expect(data.length).to.equal(count)
   })
   data.map(item => {
-    it(`should be a number less than ${max}`, () => {
-      expect(item).to.be.a('number')
-      expect(item).to.lte(max)
-    })
+    checkNumber(item, min, max)
   })
 })
 
