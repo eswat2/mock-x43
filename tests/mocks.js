7e4e91da09dd5c48e7ae09d968b3d26d588d76a4
Richard Hess <eswat2@gmail.com>
2022-06-14 15:31:20 -0700

    refactor tests

--- tests/mocks.js
+++ tests/mocks.js
@@ -10,6 +10,15 @@ import {
 } from '../utils/mocks.js'
 import { expect } from 'chai'
 
+const checkArray = (data, count) => {
+  it('should return an array', () => {
+    expect(data).to.be.a('array')
+  })
+  it(`should be ${count} items`, () => {
+    expect(data.length).to.equal(count)
+  })
+}
+
 const checkNumber = (data, min, max) => {
   it('should return a number', () => {
     expect(data).to.be.a('number')
@@ -29,76 +38,75 @@ const checkString = (data, limit) => {
   })
 }
 
-describe('chance.hash', () => {
-  const data = chance.hash()
-  const limit = 40
-  console.log('-- hash:', data)
-  checkString(data, limit)
-})
-
-describe('delay', () => {
-  const data = delay()
-  const min = 500
-  const max = 3500
-  console.log('-- delay:', data)
-  checkNumber(data, min, max)
-})
-
-describe('nid', () => {
-  const data = nid()
-  const limit = 21
-  console.log('-- nid:', data)
-  checkString(data, limit)
-})
-
-describe('randomArray', () => {
-  const count = 10
-  const min = 0
-  const max = 20
-  const data = randomArray(count, max)
-  console.log('-- randomArray:', data.toString())
-  it('should return an array', () => {
-    expect(data).to.be.a('array')
+describe('mocks', () => {
+  describe('chance.hash', () => {
+    const data = chance.hash()
+    const limit = 40
+    console.log('-- hash:', data)
+    checkString(data, limit)
   })
-  it(`should be ${count} items`, () => {
-    expect(data.length).to.equal(count)
+
+  describe('delay', () => {
+    const data = delay()
+    const min = 500
+    const max = 3500
+    console.log('-- delay:', data)
+    checkNumber(data, min, max)
   })
-  data.map((item) => {
-    checkNumber(item, min, max)
+
+  describe('nid', () => {
+    const data = nid()
+    const limit = 21
+    console.log('-- nid:', data)
+    checkString(data, limit)
   })
-})
 
-describe('sfid', () => {
-  const data = sfid()
-  const limit = 19
-  console.log('-- sfid:', data)
-  checkString(data, limit)
-})
+  describe('randomArray', () => {
+    const count = 10
+    const min = 0
+    const max = 20
+    const data = randomArray(count, max)
+    console.log('-- randomArray:', data.toString())
+    checkArray(data, count)
+    describe('items', () => {
+      data.map((item) => {
+        checkNumber(item, min, max)
+      })
+    })
+  })
 
-describe('sfid.hex', () => {
-  const data = sfid(true)
-  const limit = 18
-  console.log('-- sfid.hex:', data)
-  checkString(data, limit)
-})
+  describe('sfid', () => {
+    const data = sfid()
+    const limit = 19
+    console.log('-- sfid:', data)
+    checkString(data, limit)
+  })
 
-describe('sid', () => {
-  const data = sid()
-  const limit = 9
-  console.log('-- sid:', data)
-  checkString(data, limit)
-})
+  describe('sfid.hex', () => {
+    const data = sfid(true)
+    const limit = 18
+    console.log('-- sfid.hex:', data)
+    checkString(data, limit)
+  })
 
-describe('slug', () => {
-  const data = slug()
-  const limit = 17
-  console.log('-- slug:', data)
-  checkString(data, limit)
-})
+  describe('sid', () => {
+    const data = sid()
+    const limit = 9
+    console.log('-- sid:', data)
+    checkString(data, limit)
+  })
 
-describe('uuid', () => {
-  const data = uuid()
-  const limit = 36
-  console.log('-- uuid:', data)
-  checkString(data, limit)
+  describe('slug', () => {
+    const data = slug()
+    const limit = 17
+    console.log('-- slug:', data)
+    checkString(data, limit)
+  })
+
+  describe('uuid', () => {
+    const data = uuid()
+    const limit = 36
+    console.log('-- uuid:', data)
+    checkString(data, limit)
+  })
 })
